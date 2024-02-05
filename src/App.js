ccee72052a2f25caf586f8ca87f20d04931135ff
venkat2602 <19h61a05j4@cvsr.ac.in>
2024-02-06 00:04:38 +0530

    survey sparrow live coding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import background from "./assets/background.jpeg";
+import profile from "./assets/HITMAN.jpeg";
 
 function App() {
+  const [followersCount, setFollowersCount] = useState(248);
+  const [toggleFollow, setToggleFollow] = useState(true);
+
+  function handleFollow() {
+    if (toggleFollow) {
+      setFollowersCount(followersCount + 1);
+    } else {
+      setFollowersCount(followersCount - 1);
+    }
+    setToggleFollow(!toggleFollow);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col w-80 justify-center mx-auto my-auto shadow-lg rounded-2xl'>
+      <div className='w-full relative'>
+        <img
+          className='w-full h-40'
+          src={background}
+          alt='backgroundimage'></img>
+        <img
+          src={profile}
+          className='w-28 h-28 rounded-full absolute -bottom-12 left-1/3 right-1/3'
+          alt='profile pic'></img>
+      </div>
+      <div className='mt-10 flex flex-col justify-center items-center p-2 text-center'>
+        <h1 className='text-2xl font-semibold text-violet-400'>Rohit</h1>
+        <p className='text-slate-500'>UI/UX designer</p>
+      </div>
+      <div className='flex flex-row justify-around text-center border-y p-2 mx-2'>
+        <div>
+          <h3 className='text-slate-500'>Projects</h3>
+          <p className='text-lg font-semibold'>7</p>
+        </div>
+        <div>
+          <h3 className='text-slate-500'>Following</h3>
+          <p className='text-lg font-semibold'>{followersCount}</p>
+        </div>
+        <div>
+          <h3 className='text-slate-500'>Followers</h3>
+          <p className='text-lg font-semibold'>162</p>
+        </div>
+      </div>
+      <button
+        className='text-white bg-violet-500 rounded-lg p-2 mx-10 my-7
+        '
+        onClick={handleFollow}>
+        {toggleFollow ? "Follow" : "Unfollow"}
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='flex justify-center items-center w-full h-screen'>
+      <App />
+    </div>
   </React.StrictMode>
 );
 
